e79e05ec38614d44370847584aae4ba8477090b4
samiullahbahadur <bahadursamiullah@gmail.com>
2022-02-22 06:53:58 +0430

    Add local time

---
+++ modules/removeBook.js
@@ -0,0 +1,6 @@
+export default function removeBk() {
+  const Book = new Books();
+  Book.removeBook(this.id);
+  this.parentNode.parentNode.remove();
+  snaker("remove", "<b> Remove:</b> Book Removed successfully ", 4000);
+}
